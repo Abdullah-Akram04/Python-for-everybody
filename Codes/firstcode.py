623cb39baa70b7b4dacc4a77cce7e69ee55445c5
Abdullah Akram <ch.abdullahakram786@gmail.com>
2026-06-20 12:06:43 +0500

    count the number

--- Codes/firstcode.py
+++ Codes/firstcode.py
@@ -5,3 +5,7 @@ x = x + 15
 print(x)
 print(type(x))
 
+# count the elevator number of floors
+
+input_floor = int(input("Enter the floor number: "))
+print("The elevator will stop at floor number:", input_floor)
