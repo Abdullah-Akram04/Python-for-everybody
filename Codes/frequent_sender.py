33e61369f8afd57fd7913af9d2e9e0d6da9398b1
Abdullah Akram <ch.abdullahakram786@gmail.com>
2026-06-21 19:00:03 +0500

    sender find

---
+++ Codes/frequent_sender.py
@@ -0,0 +1,27 @@
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+
+fh = open(fname)
+
+counts = {}
+
+for line in fh:
+    if not line.startswith("From "):
+        continue
+
+    words = line.split()
+    email = words[1]
+
+    counts[email] = counts.get(email, 0) + 1
+
+# find the most prolific sender
+big_count = None
+big_email = None
+
+for email, count in counts.items():
+    if big_count is None or count > big_count:
+        big_email = email
+        big_count = count
+
+print(big_email, big_count)
